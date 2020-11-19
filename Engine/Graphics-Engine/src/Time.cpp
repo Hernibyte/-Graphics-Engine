0ebefdf28228bfff083c6b553925578295c984c4
Gossa Hernan <hernanagustin.g24@hotmail.com>
2020-11-19 15:47:43 -0300

    Added base of time class.

--- Engine/Graphics-Engine/src/Time.cpp
+++ Engine/Graphics-Engine/src/Time.cpp
@@ -1,2 +1,35 @@
 #include "Time.h"
 
+Time::Time() {
+
+}
+Time::~Time() {
+
+}
+void Time::reset() {
+
+}
+void Time::setTimeScale() {
+
+}
+float Time::getSeconds() {
+
+}
+void Time::startTime() {
+
+}
+void Time::tick() {
+
+}
+float Time::deltaTime() {
+
+}
+void Time::calculateFps() {
+
+}
+float Time::getFps() {
+
+}
+void Time::FPS() {
+
+}
--- Engine/Graphics-Engine/src/Time.h
+++ Engine/Graphics-Engine/src/Time.h
@@ -6,9 +6,25 @@
 
 class GENGINE_API Time {
 private:
-
+	std::chrono::system_clock::time_point startTime;
+	float deltaTime;
+	float timeScale;
+	float elapsedTime = 0;
+	float framesCounter;
+	int frames;
+	float fps = 0;
 public:
-
+	Time();
+	~Time();
+	void reset();
+	void setTimeScale();
+	float getSeconds();
+	void startTime();
+	void tick();
+	float deltaTime();
+	void calculateFps();
+	float getFps();
+	void FPS();
 };
 
 #endif // !TIME_H
